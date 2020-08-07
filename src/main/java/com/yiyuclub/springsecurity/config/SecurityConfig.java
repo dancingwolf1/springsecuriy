3ced3501d6750f1c76eafe1e9f58cf702c4e2559
chenshiqiang <shiqiang_chen@126.com>
2020-08-07 17:55:56 +0800

    添加配置权限的学习备注

--- src/main/java/com/yiyuclub/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/yiyuclub/springsecurity/config/SecurityConfig.java
@@ -48,19 +48,42 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                //允许所有请求通过Http Basic 验证
                 .httpBasic().authenticationEntryPoint(authenticationEntryPoint)
                 .and()
+                //访问应用的所有用户都要被验证
                 .authorizeRequests()
                 .anyRequest()
                 .access("@PermissionConfig.hasPermission(request,authentication)")
                 .and()
+                //用户可以通过表单进行验证
                 .formLogin()
+                //.loginPage( "/login")// 1 如果没有此行指定，则会使用内置的登陆页面
                 .successHandler(authenticationSuccessHandler)
                 .failureHandler(authenticationFailureHandler)
                 .permitAll()
                 .and()
+                /*注销处理
+                1、使HttpSession失效
+                2、清空已配置的RememberMe验证
+                3、清空 SecurityContextHolder
+                4、重定向到 /login?success
+                一般情况下，为了自定义注销功能，你可以添加 LogoutHandler 或者 LogoutSuccessHandler 的实现。
+                 */
+                //1、提供注销支持，当使用 WebSecurityConfigurerAdapter时这将会被自动应用
                 .logout()
+                //触发注销操作的url，默认是/logout。如果开启了CSRF保护(默认开启),那么请求必须是POST方式。
+//                .logoutUrl("/my/logout")
+                //注销操作发生后重定向到的url，默认为 /login?logout。
+//                .logoutSuccessUrl("/my/index")
+                //让你指定自定义的 LogoutSuccessHandler。如果指定了， logoutSuccessUrl() 将会被忽略。
                 .logoutSuccessHandler(logoutSuccessHandler)
+                //指定在注销的时候是否销毁 HttpSession 。默认为True。
+//                .invalidateHttpSession(true)
+                //添加一个 LogoutHandler。默认情况下， SecurityContextLogoutHandler 被作为最后一个 LogoutHandler 。
+//                .addLogoutHandler(logoutHandler)
+                //7、允许指定当注销成功时要移除的cookie的名称。这是显式添加 CookieClearingLogoutHandler 的一种快捷处理方式。
+//                .deleteCookies(cookieNamesToClear)
                 .permitAll();
 
         // 记住我
